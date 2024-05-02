c503fa9cef7ccae34e523b518d1f28a181863f6b
shlomo.bamberger <shlomo.bamberger@catonetworks.com>
2024-05-02 20:27:13 +0300

    4th commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://google.com
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
