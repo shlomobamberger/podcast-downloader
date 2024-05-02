f036600e6d1b2733ca41dcbae1c7317265e03c6c
shlomo.bamberger <shlomo.bamberger@catonetworks.com>
2024-05-02 20:23:02 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          this is me!
+          Hello!!!
         </p>
         <a
           className="App-link"
