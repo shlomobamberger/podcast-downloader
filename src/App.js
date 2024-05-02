c4698e80233059e23420574f149eaf241f3ccaca
shlomo.bamberger <shlomo.bamberger@catonetworks.com>
2024-05-02 20:20:57 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          this is me!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com
           target="_blank"
           rel="noopener noreferrer"
         >
